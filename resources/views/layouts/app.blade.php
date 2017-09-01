81fa84c49427df35e763f088bb253406e0c0fabe
Adham <adhamwynston@outlook.com>
2017-09-01 02:33:58 -0300

    Criando API para testes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,11 @@
 
         @yield('content')
     </div>
-    <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+    {{--<script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>--}}
+    <script src="{{ mix('js/app.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
     <script src="{{ asset('js/main.js') }}"></script>
-    <script src="{{ mix('js/app.js') }}"></script>
+
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+Route::view('/', 'home');
 
 Auth::routes();
 Route::get('logout', 'AuthController@getLogout');
